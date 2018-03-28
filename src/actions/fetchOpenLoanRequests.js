e490b489834690207e2de515b88ed0177a9c7314
Evgeniy Loginov <loginov_evgeniy@tut.by>
2018-03-28 21:22:43 +0300

    connect to dharma api

--- src/actions/fetchOpenLoanRequests.js
+++ src/actions/fetchOpenLoanRequests.js
@@ -1,6 +1,7 @@
 import debtsApi from '../common/api/debts';
 import * as loanStatuses from '../common/loanStatuses';
 import web3 from '../common/services/web3Service';
+import {fromDebtOrder} from '../common/services/dharmaService';
 
 export const FETCH_OPEN_LOAN_REQUESTS = 'FETCH_OPEN_LOAN_REQUESTS';
 export const FETCH_OPEN_LOAN_REQUESTS_SUCCESS = 'FETCH_OPEN_LOAN_REQUESTS_SUCCESS';
@@ -20,6 +21,12 @@ export function fetchOpenLoanRequests(){
     dispatch(fetchOpenLoanRequestsStart());
 
     return debtsApi.getAll(web3.eth.defaultAccount, loanStatuses.SIGNED_BY_DEBTOR)
-      .then(debts => {dispatch(fetchOpenLoanRequestsSuccess(debts))});
+      .then(async(debts) => {
+        let mappedDebts = [];
+        for(var i=0; i<debts.length; i++){
+          //mappedDebts.push(await fromDebtOrder(debts[i]));
+        }
+        dispatch(fetchOpenLoanRequestsSuccess(mappedDebts));
+      });
   }
 }
