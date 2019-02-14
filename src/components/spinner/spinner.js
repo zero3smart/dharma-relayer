1bf4096e6da20ec73ce3670e8aa5e58dbfeb74a8
zero2smart <fivewisdomzero@gmail.com>
2019-02-14 14:24:50 -0500

    fix warnings spinner

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -5,10 +5,10 @@ class Spinner extends Component {
   render() {
 
     return (
-      <div class="spinner">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>
+      <div className="spinner">
+        <div className="bounce1"></div>
+        <div className="bounce2"></div>
+        <div className="bounce3"></div>
       </div>
     );
   }
