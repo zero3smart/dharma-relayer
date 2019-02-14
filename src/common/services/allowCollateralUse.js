ea61446391b772fd6ebfba8650143c6125f15c76
zero2smart <fivewisdomzero@gmail.com>
2019-02-13 19:16:24 -0500

    allowCollateralUse refactoring

--- src/common/services/allowCollateralUse.js
+++ src/common/services/allowCollateralUse.js
@@ -1,40 +1,18 @@
-import web3, {getNetwork} from './web3Service';
-//import data from '../DummyToken.json';
-import DAI from '../tokenJson/DAI.json';
-import MKR from '../tokenJson/MKR.json';
-import REP from '../tokenJson/REP.json';
-import ZRX from '../tokenJson/ZRX.json';
-import * as currencyCodes from '../currencyCodes';
+import {
+  getNetwork,
+  getDefaultAccount
+} from './web3Service';
+import {
+  getTokenContractBySymbolAsync
+} from './tokenService'
 
-import contract from 'truffle-contract';
+export default async function (amount, token) {
 
-export default async function(amount, token){
-  let provider = web3.currentProvider;
-  let tokenSource = getTokenSource(token);
-  if(!tokenSource){
-    throw new Error(`Configuration for token ${token} wasn't found`);
-  }
+  const currentNetwork = await getNetwork();
+  const tokenContract = await getTokenContractBySymbolAsync(token)
+  const spender = tokenContract.address
 
-  let MyContract = contract(tokenSource);
-  MyContract.setProvider(provider);
-  let deployedContract = await MyContract.deployed();
-  let currentNetwork = await getNetwork();
-
-  let spender = tokenSource.networks[currentNetwork].address;
-  await deployedContract.approve(spender, amount, {from: web3.eth.defaultAccount});
-}
-
-function getTokenSource(token){
-  switch(token){
-    case currencyCodes.DAI:
-      return DAI;
-    case currencyCodes.MKR:
-      return MKR;
-    case currencyCodes.REP:
-      return REP;
-    case currencyCodes.ZRX:
-      return ZRX;
-    default:
-      return null;
-  }
+  await tokenContract.approve(spender, amount, {
+    from: getDefaultAccount()
+  });
 }
---
+++ src/common/services/tokenService.js
@@ -0,0 +1,48 @@
+import contract from 'truffle-contract';
+import BigNumber from 'bignumber.js';
+import DAI from '../tokenJson/DAI.json';
+import MKR from '../tokenJson/MKR.json';
+import REP from '../tokenJson/REP.json';
+import ZRX from '../tokenJson/ZRX.json';
+import * as currencyCodes from '../currencyCodes';
+import web3 from './web3Service.js'
+
+export async function convertToHumanReadable(amount, tokenSymbol) {
+    const decimals = await getTokenDecimals(tokenSymbol);
+    const res = new BigNumber(amount || 0).mul('1e-' + decimals.toNumber())
+    return res;
+}
+
+export async function convertFromHumanReadable(amount, tokenSymbol) {
+    const decimals = await getTokenDecimals(tokenSymbol);
+    const res = new BigNumber(amount || 0).mul('1e' + decimals.toNumber())
+    return res;
+}
+
+export function getTokenContractBySymbolAsync(symbol) {
+    const TokenContract = contract(getTokenSource(symbol));
+    TokenContract.setProvider(web3.currentProvider);
+
+    return TokenContract.deployed();
+}
+
+async function getTokenDecimals(symbol) {
+    const token = await getTokenContractBySymbolAsync(symbol);
+
+    return token.decimals();
+}
+
+function getTokenSource(token) {
+    switch (token) {
+        case currencyCodes.DAI:
+            return DAI;
+        case currencyCodes.MKR:
+            return MKR;
+        case currencyCodes.REP:
+            return REP;
+        case currencyCodes.ZRX:
+            return ZRX;
+        default:
+            throw new Error(`Configuration for token ${token} wasn't found`);
+    }
+}
