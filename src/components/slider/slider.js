83b5a2060c324bf9150934d827501ece4230fc4f
zero2smart <fivewisdomzero@gmail.com>
2019-02-25 20:45:03 -0500

    add colors to slider, fix react warning

--- src/components/slider/slider.js
+++ src/components/slider/slider.js
@@ -5,8 +5,8 @@ export default function (props) {
   let onChange = props.onChange || (() => { });
   return (
     <label className="switch">
-      <input type="checkbox" checked={props.on} onChange={onChange} />
-      <span className="slider round"></span>
+      <input type="checkbox" checked={!!props.on} onChange={onChange} />
+      <span className={"slider round " + (props.on ? "slider_on" : "slider_off")}></span>
     </label>
   );
 }
