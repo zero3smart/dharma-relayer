f1509fea3bc3cad87a0948e1819f059fa85d7f96
zero3smart <fivewisdomzero@gmail.com>
2019-03-07 21:19:56 -0500

    swagger link fix

--- src/common/api/config.js
+++ src/common/api/config.js
@@ -4,8 +4,8 @@ export const RELAYER_ADDRESS = '0x00F34Ad48A326b406bf995E04189e42D285d5773'
 export const RELAYER_FEE = new BigNumber(0)
 export const SHOW_LOANSCAN_LINK = false;
 
-export const HOST_URL = "http://dharma-relayer-prod-993135204.us-east-1.elb.amazonaws.com/";
-export const API_URL = HOST_URL + "api/v0";
+export const HOST_URL = "http://dharma-relayer-prod-993135204.us-east-1.elb.amazonaws.com";
+export const API_URL = HOST_URL + "/api/v0";
 export const LOANSCAN_URL = "http://loanscan.io/loan";
 
 export const SUPPORTED_TOKENS = ['REP', 'ZRX', 'MKR']
