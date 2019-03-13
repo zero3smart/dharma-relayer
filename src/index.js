8028398107e62f86aaacf977c003af62e2bcc93f
zero3smart <fivewisdomzero@gmail.com>
2019-03-13 05:32:13 -0400

    current environment log

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
+console.log(`Environment: ${process.env.NODE_ENV}`)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
