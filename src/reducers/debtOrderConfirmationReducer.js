11aaee78645f71906739204efd5b35e2e6978b8e
zero3smart <fivewisdomzero@gmail.com>
2019-03-23 04:57:21 -0400

    dev-1: merge fixes

--- src/reducers/debtOrderConfirmationReducer.js
+++ src/reducers/debtOrderConfirmationReducer.js
@@ -6,7 +6,9 @@ import {
     LOCK_COLLATERAL_TOKEN_SUCCESS,
     UNLOCK_COLLATERAL_TOKEN,
     LOCK_COLLATERAL_TOKEN,
-    RESET_LOAN_FORM
+    RESET_LOAN_FORM,
+    GET_COLLATERAL_TOKEN_LOCK,
+    GET_COLLATERAL_TOKEN_LOCK_SUCCESS,
 } from '../actions';
 
 const initialState = {
@@ -33,6 +35,9 @@ export default function (state = initialState, action) {
             return { ...state, unlockInProgress: true };
         case RESET_LOAN_FORM:
             return { ...initialState };
+        case GET_COLLATERAL_TOKEN_LOCK_SUCCESS:
+            return { ...state, collateralTokenUnlocked: action.unlocked };
+        case GET_COLLATERAL_TOKEN_LOCK:
         default:
             return state;
     }
