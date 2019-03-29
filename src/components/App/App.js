9eb4be9e265f65308a3f11da0568f3574ec027ee
zero3smart <fivewisdomzero@gmail.com>
2019-03-28 22:22:49 -0400

    chore: add redux_devtools

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,39 +7,40 @@ import './App.css';
 import UserInfo from '../user-info/user-info.js';
 import LoanRequests from '../../containers/loan-requests/loan-requests';
 import IssuedLoans from '../../containers/issued-loans/issued-loans';
-import { HOST_URL } from '../../common/api/config'
+import {HOST_URL} from '../../common/api/config'
 
 export const store = createStore(
-  rootReducer,
-  applyMiddleware(
-    thunkMiddleware
-  ));
+	rootReducer,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+	applyMiddleware(
+		thunkMiddleware
+	));
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="app">
-          <div className="app__container container-fluid">
-            <div className="row flex-sm-nowrap">
-              <div className="app__content-left col-sm">
-                <UserInfo />
+	render() {
+		return (
+            <Provider store={store}>
+              <div className="app">
+                <div className="app__container container-fluid">
+                  <div className="row flex-sm-nowrap">
+                    <div className="app__content-left col-sm">
+                      <UserInfo />
+                    </div>
+                    <div className="app__content-center col-sm">
+                      <LoanRequests />
+                    </div>
+                    <div className="app__content-right col-sm">
+                      <IssuedLoans />
+                    </div>
+                  </div>
+                </div>
+                <div className="app__demo-link">
+                  <a href={`${HOST_URL}/swagger/`} target="_blank">API Documentation</a>
+                </div>
               </div>
-              <div className="app__content-center col-sm">
-                <LoanRequests />
-              </div>
-              <div className="app__content-right col-sm">
-                <IssuedLoans />
-              </div>
-            </div>
-          </div>
-          <div className="app__demo-link">
-            <a href={`${HOST_URL}/swagger/`} target="_blank">API Documentation</a>
-          </div>
-        </div>
-      </Provider>
-    );
-  }
+            </Provider>
+		);
+	}
 }
 
 export default App;
--- src/containers/outstanding-loans/outstanding-loans.js
+++ src/containers/outstanding-loans/outstanding-loans.js
@@ -9,110 +9,119 @@ const pageSize = 5;
 
 let destroyTimer = null;
 let startTimer = (func) => {
-  destroyTimer = setTimeout(() => {
-    func();
-    startTimer(func);
-  }, 10000)
+	destroyTimer = setTimeout(() => {
+		func();
+		startTimer(func);
+	}, 10000)
 };
 
 class OutstandingLoans extends Component {
-  state = {
-    loan: null,
-    isRepayModalOpened: false,
-  }
-
-  constructor(props) {
-    super(props);
-
-    this.getOutstandingLoansForCurrentPage = this.getOutstandingLoansForCurrentPage.bind(this);
-  }
-
-  componentDidMount() {
-    let { getOutstandingLoansForCurrentPage } = this;
-    getOutstandingLoansForCurrentPage();
-    startTimer(getOutstandingLoansForCurrentPage);
-  }
-
-  componentWillUnmount() {
-    destroyTimer && destroyTimer();
-  }
-
-  getOutstandingLoansForCurrentPage() {
-    let { offset, fetchMyOutstandingLoans } = this.props;
-    let currentPageNum = Math.floor(offset / pageSize);
-
-    fetchMyOutstandingLoans(pageSize * currentPageNum, pageSize);
-  }
-
-  handleOpenModal = () =>
-    this.setState(prevState => ({
-      isRepayModalOpened: true
-    })
-    )
-
-  handleCloseModal = () =>
-    this.setState(prevState => ({
-      isRepayModalOpened: false
-    })
-    )
-
-  handleRepayModal = loan => {
-    this.setState(prevState => ({ loan }))
-    this.handleOpenModal()
-  }
-
-  onRepay = ({ issuanceHash, amount, token }) => {
-    this.handleCloseModal()
-    repayLoan(issuanceHash, amount, token)
-  }
-
-  render() {
-    let { myOutstandingLoans, showPaging, isLoading, offset, totalItemsCount, setMyOutstandingLoansOffset, fetchMyOutstandingLoans } = this.props;
-
-    let rows = myOutstandingLoans.map(loan => ({
-      date: new Date(loan.issuanceBlockTime),
-      principalAmount: loan.principalAmount.toNumber(),
-      principalTokenSymbol: loan.principalTokenSymbol,
-      termLength: loan.termLength.toNumber(),
-      amortizationUnit: loan.amortizationUnit,
-      interestRate: loan.interestRate,
-      issuanceHash: loan.issuanceHash
-    }));
-
-    return (
-      <Fragment>
-        <LoanTableSmall
-          header="My outstanding loans"
-          dateColumnHeader="Date loan issued"
-          repayAvailable={true}
-          handleRepay={this.handleRepayModal}
-          rows={rows}
-          isLoading={isLoading}
-          showPaging={showPaging}
-          offset={offset}
-          totalItemsCount={totalItemsCount}
-          pageSize={pageSize}
-          onPageClick={(pageNum) => {
-            setMyOutstandingLoansOffset(pageSize * pageNum);
-            fetchMyOutstandingLoans(pageSize * pageNum, pageSize);
-          }} />
-        <RepayLoanModal
-          loan={this.state.loan}
-          isOpen={this.state.isRepayModalOpened}
-          handleClose={this.handleCloseModal}
-          onRepay={this.onRepay}
-        />
-      </Fragment>
-    );
-  }
+	state = {
+		loan: null,
+		isRepayModalOpened: false,
+		loading: false,
+	}
+	
+	constructor(props) {
+		super(props);
+		
+		this.getOutstandingLoansForCurrentPage = this.getOutstandingLoansForCurrentPage.bind(this);
+	}
+	
+	componentDidMount() {
+		let { getOutstandingLoansForCurrentPage } = this;
+		getOutstandingLoansForCurrentPage();
+		startTimer(getOutstandingLoansForCurrentPage);
+	}
+	
+	componentWillUnmount() {
+		destroyTimer && destroyTimer();
+	}
+	
+	getOutstandingLoansForCurrentPage() {
+		let { offset, fetchMyOutstandingLoans } = this.props;
+		let currentPageNum = Math.floor(offset / pageSize);
+		
+		fetchMyOutstandingLoans(pageSize * currentPageNum, pageSize);
+	}
+	
+	handleOpenModal = () =>
+		this.setState(prevState => ({
+				isRepayModalOpened: true
+			})
+		)
+	
+	handleCloseModal = () =>
+		this.setState(prevState => ({
+				isRepayModalOpened: false
+			})
+		)
+	
+	handleRepayModal = loan => {
+		this.setState(prevState => ({ loan }))
+		this.handleOpenModal()
+	}
+	
+	onRepay = ({ issuanceHash, amount, token }) => {
+		this.setState({ loading: true })
+		repayLoan(issuanceHash, amount, token)
+			.then(loan => {
+				this.handleCloseModal()
+				this.setState({ loading: false })
+			})
+			.catch(err => {
+				console.error(err)
+				this.setState({ loading: false })
+			})
+	}
+	
+	render() {
+		let { myOutstandingLoans, showPaging, isLoading, offset, totalItemsCount, setMyOutstandingLoansOffset, fetchMyOutstandingLoans } = this.props;
+		
+		let rows = myOutstandingLoans.map(loan => ({
+			date: new Date(loan.issuanceBlockTime),
+			principalAmount: loan.principalAmount.toNumber(),
+			principalTokenSymbol: loan.principalTokenSymbol,
+			termLength: loan.termLength.toNumber(),
+			amortizationUnit: loan.amortizationUnit,
+			interestRate: loan.interestRate,
+			issuanceHash: loan.issuanceHash
+		}));
+		
+		return (
+            <Fragment>
+              <LoanTableSmall
+                  header="My outstanding loans"
+                  dateColumnHeader="Date loan issued"
+                  repayAvailable={true}
+                  handleRepay={this.handleRepayModal}
+                  rows={rows}
+                  isLoading={isLoading}
+                  showPaging={showPaging}
+                  offset={offset}
+                  totalItemsCount={totalItemsCount}
+                  pageSize={pageSize}
+                  onPageClick={(pageNum) => {
+					  setMyOutstandingLoansOffset(pageSize * pageNum);
+					  fetchMyOutstandingLoans(pageSize * pageNum, pageSize);
+				  }}/>
+              <RepayLoanModal
+                  loan={this.state.loan}
+                  isOpen={this.state.isRepayModalOpened}
+                  handleClose={this.handleCloseModal}
+                  onRepay={this.onRepay}
+              />
+            </Fragment>
+		);
+	}
 }
 
-let mapStateToProps = ({ myOutstandingLoans: { values, isLoading, offset, showPaging, totalItemsCount } }) => ({
-  myOutstandingLoans: values,
-  isLoading,
-  offset,
-  showPaging,
-  totalItemsCount
+let mapStateToProps = ({ myOutstandingLoans:{ values, isLoading, offset, showPaging, totalItemsCount } }) => ({
+	myOutstandingLoans: values,
+	isLoading,
+	offset,
+	showPaging,
+	totalItemsCount
 });
 
 let mapDispatchToProps = { fetchMyOutstandingLoans, setMyOutstandingLoansOffset };
