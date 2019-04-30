eb0797fbcf8d3410d9c46a5ab634f258de3f3b0d
zero3smart <fivewisdomzero@gmail.com>
2019-04-30 12:03:29 -0300

    RepayLoanModal fix

--- src/containers/outstanding-loans/RepayLoanModal.js
+++ src/containers/outstanding-loans/RepayLoanModal.js
@@ -10,172 +10,172 @@ import { formatLoanscanLink } from '../../common/services/utilities';
 import { SHOW_LOANSCAN_LINK } from '../../common/api/config';
 
 const initialState = {
-    amount: "",
-    owe: ""
+  amount: "",
+  oweAmount: ""
 };
 
 let timer = null;
 
 let startTimer = (func) => {
-    timer = setTimeout(() => {
-        func();
-        startTimer(func);
-    }, 5000)
+  timer = setTimeout(() => {
+    func();
+    startTimer(func);
+  }, 5000)
 };
 
 class RepayLoanModal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
-    }
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
 
 
-    componentDidMount() {
-        this.props.getWalletInfo();
-        this.updateYourOwe();
-        startTimer(this.updateYourOwe);
-    }
+  componentDidMount() {
+    this.props.getWalletInfo();
+    this.updateYourOwe();
+    startTimer(this.updateYourOwe);
+  }
 
-    componentWillUnmount() {
-        timer && clearTimeout(timer);
-    }
+  componentWillUnmount() {
+    timer && clearTimeout(timer);
+  }
 
-    updateYourOwe = () => {
-        getRemainingRepaymentValue(this.props.loan)
-            .then(res => {
-                const owe = res.toFormat(5);
-                this.setState({
-                    owe: `${owe} ${this.props.loan.principalTokenSymbol}`
-                })
-            })
-    };
-
-    getFormattedIssuanceHash = issuanceHash =>
-        `${issuanceHash.substr(0, 5)}...${issuanceHash.substr(-5)}`;
-
-    onAmountChange = ({ target }) => {
-        this.setState({ amount: target.value })
-    };
-
-    onTokenChange = ({ target }) => {
-        this.setState({ token: target.value });
-        this.props.selectCurrency(target.value)
-    };
-
-    handleRepay = () => {
-        this.props.onRepay({
-            issuanceHash: this.props.loan.issuanceHash,
-            token: this.props.loan.principalTokenSymbol,
-            ...this.state,
+  updateYourOwe = () => {
+    getRemainingRepaymentValue(this.props.loan)
+      .then(res => {
+        const oweAmount = res.toFormat(5);
+        this.setState({
+          oweAmount
         })
-    };
+      })
+  };
 
-    onClose = () => {
-        this.setState(initialState);
-        this.props.handleClose();
-    };
+  getFormattedIssuanceHash = issuanceHash =>
+    `${issuanceHash.substr(0, 5)}...${issuanceHash.substr(-5)}`;
 
-    renderFirstStep() {
-        const { loan, isLoading } = this.props;
+  onAmountChange = ({ target }) => {
+    this.setState({ amount: target.value })
+  };
 
-        return (
-            <div className="confirm">
-                <div className="confirm__row">
-                    <div className="confirm__header">
-                        <h1>Make Repayment</h1>
-                    </div>
-                </div>
-                <div className="confirm__row">
-                    <div>
-                        You are making a repayment for debt agreement
+  onTokenChange = ({ target }) => {
+    this.setState({ token: target.value });
+    this.props.selectCurrency(target.value)
+  };
+
+  handleRepay = () => {
+    this.props.onRepay({
+      issuanceHash: this.props.loan.issuanceHash,
+      token: this.props.loan.principalTokenSymbol,
+      ...this.state,
+    })
+  };
+
+  onClose = () => {
+    this.setState(initialState);
+    this.props.handleClose();
+  };
+
+  renderFirstStep(){
+    const { loan, isLoading } = this.props;
+
+    return (
+      <div className="confirm">
+        <div className="confirm__row">
+          <div className="confirm__header">
+            <h1>Make Repayment</h1>
+          </div>
+        </div>
+        <div className="confirm__row">
+          <div>
+            You are making a repayment for debt agreement
             {
-                            SHOW_LOANSCAN_LINK ?
-                                <a href={formatLoanscanLink(loan.issuanceHash)} target="_blank"> {loan.issuanceHash && this.getFormattedIssuanceHash(loan.issuanceHash)}</a> :
-                                <strong> {loan.issuanceHash && this.getFormattedIssuanceHash(loan.issuanceHash)}</strong>
-                        }.
+              SHOW_LOANSCAN_LINK ?
+                <a href={formatLoanscanLink(loan.issuanceHash)} target="_blank"> {loan.issuanceHash && this.getFormattedIssuanceHash(loan.issuanceHash)}</a> :
+                <strong> {loan.issuanceHash && this.getFormattedIssuanceHash(loan.issuanceHash)}</strong>
+            }.
           </div>
-                    <div>
-                        You owe: {!this.state.owe ? <Spinner size="5px" class="inline" /> : <strong>{this.state.owe}</strong>}
-                    </div>
-                    <div>
-                        How large of repayment would you like to make?
+          <div>
+            You owe: { !this.state.oweAmount ? <Spinner size="5px" class="inline" /> : <strong>{this.state.oweAmount} {this.props.loan.principalTokenSymbol}</strong> }
           </div>
-                </div>
-                <br />
-                <div className="confirm__row">
-                    <input
-                        type="text"
-                        placeholder="Amount (e.g. 12.32)"
-                        name="amount"
-                        value={this.state.amount}
-                        onChange={this.onAmountChange}
-                    />
-                    <input type="text"
-                        className="repay-modal-token-symbol"
-                        disabled
-                        defaultValue={loan.principalTokenSymbol}
-                    />
-                </div>
-                <div className="confirm__buttons">
-                    <div className="confirm__btn-wrapper">
-                        <button
-                            className="confirm__btn confirm__btn_cancel"
-                            onClick={this.onClose}
-                        >
-                            CANCEL
+          <div>
+            How large of repayment would you like to make?
+          </div>
+        </div>
+        <br/>
+        <div className="confirm__row">
+          <input
+            type="text"
+            placeholder="Amount (e.g. 12.32)"
+            name="amount"
+            value={this.state.amount}
+            onChange={this.onAmountChange}
+          />
+          <input type="text"
+                 className="repay-modal-token-symbol"
+                 disabled
+                 defaultValue={loan.principalTokenSymbol}
+          />
+        </div>
+        <div className="confirm__buttons">
+          <div className="confirm__btn-wrapper">
+            <button
+              className="confirm__btn confirm__btn_cancel"
+              onClick={this.onClose}
+            >
+              CANCEL
             </button>
-                        <button
-                            className={`confirm__btn confirm__btn_confirm ${!this.state.amount ? "disabled" : ""}`}
-                            disabled={!this.state.amount}
-                            onClick={this.handleRepay}
-                        >
-                            {
-                                isLoading
-                                    ? (
-                                        <div className="confirm-btn-spinner">
-                                            <Spinner size="13px" />
-                                        </div>)
-                                    : <span>
-                                        MAKE REPAYMENT
+            <button
+              className={`confirm__btn confirm__btn_confirm ${!this.state.amount ? "disabled" : ""}`}
+              disabled={!this.state.amount}
+              onClick={this.handleRepay}
+            >
+              {
+                isLoading
+                  ? (
+                  <div className="confirm-btn-spinner">
+                    <Spinner size="13px"/>
+                  </div>)
+                  : <span>
+                          MAKE REPAYMENT
                         </span>
-                            }
-                        </button>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+              }
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 
-    renderSecondStep() {
-        return (
-            <div>
-                <div className="confirm">
-                    <div className="confirm__row">
-                        <div className="confirm__header"><h5>Successfully made repayment of {this.state.amount} MKR</h5></div>
-                    </div>
-                    <div className="confirm__buttons">
-                        <div className="confirm__btn-wrapper confirm__btn-wrapper_centered">
-                            <button
-                                className="confirm__btn confirm__btn_confirm"
-                                onClick={this.onClose}>
-                                OK
+  renderSecondStep(){
+    return (
+      <div>
+        <div className="confirm">
+          <div className="confirm__row">
+            <div className="confirm__header"><h5>Successfully made repayment of {this.state.amount} {this.props.loan.principalTokenSymbol}</h5></div>
+          </div>
+          <div className="confirm__buttons">
+            <div className="confirm__btn-wrapper confirm__btn-wrapper_centered">
+              <button
+                className="confirm__btn confirm__btn_confirm"
+                onClick={this.onClose}>
+                OK
               </button>
-                        </div>
-                    </div>
-                </div>
             </div>
-        );
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  render() {
+    const { repayLoanComplete } = this.props;
+
+    if(repayLoanComplete && this.state.amount){
+      return this.renderSecondStep();
     }
 
-    render() {
-        const { repayLoanComplete } = this.props;
-
-        if (repayLoanComplete && this.state.amount) {
-            return this.renderSecondStep();
-        }
-
-        return this.renderFirstStep();
-    }
+    return this.renderFirstStep();
+  }
 }
 
 const mapStateToProps = ({ walletInfo }) => ({ ...walletInfo });
