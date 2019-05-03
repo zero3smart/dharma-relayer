4fc305edda15e16350454b078e7406355f4db814
zero3smart <fivewisdomzero@gmail.com>
2019-05-03 03:14:18 -0300

    typo fix

--- src/components/disclosure-modal/DisclosureModal.js
+++ src/components/disclosure-modal/DisclosureModal.js
@@ -27,7 +27,7 @@ class DisclosureModal extends Component{
           <div className="confirm__row">
             <div className="confirm__header"><h5>Main disclosure</h5></div>
           </div>
-          <p>BloqBoard is a free software-based tool intended to help users to create and display their loan requests for ERC20-compatible blockchain tokens issued in compliance with the Dharma Protocol on a purely peer-to-peer basis. BloqBeacon is not a regulated marketplace, exchange or intermediary of any kind and is merely an aggregator of information.</p>
+          <p>BloqBoard is a free software-based tool intended to help users to create and display their loan requests for ERC20-compatible blockchain tokens issued in compliance with the Dharma Protocol on a purely peer-to-peer basis. BloqBoard is not a regulated marketplace, exchange or intermediary of any kind and is merely an aggregator of information.</p>
           <p>BloqBoard is not a party to any contract entered into between users of the Dharma Protocol, does not act as a lender or give loans using the Dharma Protocol, and does not otherwise enter into any agreements with or commit to any obligations to any user of the Dharma Protocol. Further, you acknowledge that the Dharma Protocol is in beta testing, may have limited functionality, and may contain errors.</p>
           <p>By clicking “I Agree” below, you understand that you are solely responsible for interacting with smart contracts deployed by third parties on the Ethereum blockchain and are in compliance with all applicable laws and regulations.</p>
           <div className="unlock-tokens__toggle-wrapper">
